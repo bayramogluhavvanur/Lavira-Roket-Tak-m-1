17e9e9383fa0487139a7c4287ae9db6c17c6f6d9
bayramogluhavvanur <bayramogluhavvanur2006@gmail.com>
2024-10-09 17:13:44 +0300

    Add files via upload

---
+++ my project 2/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+
+1- Each team can only play 1 match that week
+2- A team has to play a match with the opposing team both at home and away once
+(For example, the Fenerbahce - Besiktas match should be played both in Kadikoy and in Besiktas.)
+3- Each team has to play both home and away matches with all other competing teams.
+4- Each match must be scheduled in the form of a date and time.There may be more than one match on the same date, but not more than one at the same time.
+5- There should be at least 3 hours between each match Dec.(in other words, a maximum of 8 matches can be played in one day)
+6- İn other words, a maximum of 8 matches can be played in one day.
+
+
+*/
+
+
+int main()
+
+
+    char Team ='team1';
+    char Team ='team2';
+    char Team ='team3';
+    char Team ='team4';
+    char Team ='team5';
+    char Team ='team6';
+    char Team ='team7';
+    char Team ='team8';
+    char Team ='team9';
+    char Team ='team10';
+    char Team ='team11';
+    char Team ='team12';
+    char Team ='team13';
+    char Team ='team14';
+    char Team ='team15';
+    char Team ='team16';
+    char Team ='team17';
+    char Team ='team18';
+    char Team ='team19';
+    char Team ='team20';
+
+
+    printf( "Match the entered teams so that the date and time are appropriate\n");
+    scanf("%c",&Team);
+
+
+    return 0;
+}
